bb9b036ffcda7ecc1976c658826f3209db5d5f87
Esmeralda33 <ebarbacil33@gmail.com>
2017-03-21 18:15:41 -0300

    only init conditions

---
+++ tp3.py
@@ -0,0 +1,14 @@
+# Initial conditions: Sumatoria de tiempo de salida, llegada, cantidad de
+# llegadas, sumatoria de tiempo ocioso
+STS, STLL, CLL, STO = 0, 0, 0, 0
+
+
+# TPS/TPLL : Tiempo proxima salida/llegada
+# NS : Numero de personas en el sistema
+t = 0
+ITO = 0
+NS = 0
+TPLL = 0
+# TPS = Higher Value
+
+
